4bcffc364666c52eead1e81a172575035b983315
Marwa Derbel <marwaderbel21@gmail.com>
2024-10-01 14:04:44 +0200

    meta tag

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -28,9 +28,11 @@ function Navbar() {
         <a className="navbar-brand" href="#home">
           <img src={logo} alt="Logo" className="logo" />
         </a>
+
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
+        
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
